6e5cb740843b828b6036ca3865ad38d3235562f5
daniel-macias <josedanielmr@gmail.com>
2025-01-10 04:03:40 -0600

    trying to make pet eat food, not dragging

--- Scripts/Pet.gd
+++ Scripts/Pet.gd
@@ -2,7 +2,10 @@ extends Node2D
 
 @onready var animation_player_arms = $Arms/AnimationPlayerArms
 @onready var animation_player_body = $AnimationPlayerBody
+@onready var animation_player_eyes = $Body/AnimationPlayerEyes
+@onready var interaction_space = $InteractSpace
 
 func _ready():
 	animation_player_arms.play("Idle")
 	animation_player_body.play("Idle")
+	animation_player_eyes.play("Idle")
--- Scripts/UIManagers/FoodGUI.gd
+++ Scripts/UIManagers/FoodGUI.gd
@@ -4,9 +4,15 @@ extends Control
 @onready var food_arrow_right = $HBoxContainer/FoodArrowRight
 @onready var food_selected_text = $HBoxContainer/FoodSelected/FoodSelectedName
 @onready var food_selected = $HBoxContainer/FoodSelected
+@onready var draggable_food = $"../../DraggableFood"
 
 var food_list: Array = []
 var current_food_index: int = 0
+
+var current_food_dragged_key : String = ""
+var dragging = false
+var drag_start_position = Vector2()
+var drag_threshold = 100  # Define the threshold in pixels
 
 func _ready() -> void:
 	# Load the food inventory from the GameManager
@@ -18,6 +24,9 @@ func _ready() -> void:
 	
 	# Display the initial food item
 	_update_food_display()
+	
+	# Hide the draggable sprite initially
+	draggable_food.visible = false
 
 func _on_left_arrow_pressed() -> void:
 	# Move to the previous food item
@@ -31,6 +40,7 @@ func _on_right_arrow_pressed() -> void:
 
 func _update_food_display():
 	if food_list.size() > 0:
+		food_selected.disabled = false
 		var current_food = food_list[current_food_index]
 		var food_details = Catalog.get_food_details(current_food) # Using the catalog to get food details
 
@@ -43,3 +53,27 @@ func _update_food_display():
 	else:
 		print("No food items in inventory!")
 		food_selected_text.text = "Empty"
+		food_selected.disabled = true
+
+# Input handling
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		if event.is_pressed():
+			# When touch begins
+			dragging = true
+			drag_start_position = event.position
+			draggable_food.visible = true
+			draggable_food.position = food_selected.position  # Set sprite to the position of the button
+
+		if event.is_released():
+			# When touch ends
+			dragging = false
+			draggable_food.visible = false
+
+		if dragging:
+			var drag_distance = event.position - drag_start_position
+			draggable_food.position = food_selected.position + drag_distance
+			print(drag_distance)
+			if drag_distance.length() > drag_threshold:
+				# Change scale or any other action when threshold is reached
+				draggable_food.scale = 1.5 
